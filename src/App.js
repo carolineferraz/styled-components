95844aa01b7f2d2fa43137c1ab7ea14b0ac960af
Caroline Ferraz <projetos.crrz@gmail.com>
2022-12-08 21:04:30 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Titulo = styled.h1`
+  color: #31e36;
+`
+const Subtitulo = styled.h2`
+  color: green;
+`
+const Artigo = styled.section`
+  color: #9393e2;
+`
+const Subtitulo2 = styled.h2`
+  color: blue;
+  font-size: 25px;
+`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Titulo>Meu site</Titulo>
+        <Subtitulo>Subtitulo</Subtitulo>
+        <Artigo>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec molestie libero, nec pulvinar erat. Cras ut mattis ante. Proin massa neque, aliquam eget aliquam et, lobortis venenatis dolor. Cras quis vehicula odio. Mauris non blandit urna. Ut vulputate elit eu pharetra rhoncus. Cras mollis ligula gravida sem blandit lobortis ullamcorper et dui.</Artigo>
+        <Subtitulo2>Segunda Parte</Subtitulo2>
+        <Artigo>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec molestie libero, nec pulvinar erat. Cras ut mattis ante. Proin massa neque, aliquam eget aliquam et, lobortis venenatis dolor. Cras quis vehicula odio. Mauris non blandit urna. Ut vulputate elit eu pharetra rhoncus. Cras mollis ligula gravida sem blandit lobortis ullamcorper et dui.</Artigo>
       </header>
     </div>
   );
